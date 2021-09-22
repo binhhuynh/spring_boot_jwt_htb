e2d5f5540bc8450c8d4f37fae347709d7d103852
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-09-22 10:27:57 +0700

    Initial commit

---
+++ src/main/java/com/htb/SpringBootJwtHtbApplication.java
@@ -0,0 +1,13 @@
+package com.htb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtHtbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtHtbApplication.class, args);
+    }
+
+}
